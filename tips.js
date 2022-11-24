0de4113f5987695a401fdc23588d1fbe86f4d2cb
jwrdwn <105725028+jwrdwn@users.noreply.github.com>
2022-11-24 10:29:37 -0300

    Comandos úteis

---
+++ tips.js
@@ -0,0 +1,24 @@
+// parseInt(x), parseFloat(x), Number(x)
+/*
+    Converte string para número
+*/
+
+// x.toUpperCase(), x.toLowerCase()
+/*
+    Converte string para maiúscula ou minúscula
+*/
+
+// x.toFixed(n)
+/*
+    Mostra n casas decimais
+*/
+
+// x.replace('y', 'z')
+/*
+    Substitui o caractere y por z
+*/
+
+// x.toLocaleString('pt-BR', {style: 'currency', 'BRL'})
+/*
+    Apresenta uma string monetária. Pode ser usado com USD, EUR, etc.
+*/
